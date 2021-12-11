58c9455ad5b3b0961fae8fdb1533382822f81ae9
Albert Lee <leesga8@gmail.com>
2021-12-10 16:06:46 -0800

    add detail to UI program.cs

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -10,15 +10,18 @@ namespace Project
     {
       Console.WriteLine("Welcome to Pierre's Bakery!");
       Console.WriteLine("---------------------------");
-      Console.WriteLine("Please enter a whole number of bread loaves you'd like: ");
+      Console.WriteLine("Please enter the number of bread loaves you'd like to order. Only whole loaves please: ");
       int numBread = int.Parse(Console.ReadLine());
-      Console.WriteLine("Please enter a whole number of pastries you'd like: ");
+      Console.WriteLine("---------------------------");
+      Console.WriteLine("Please enter a the number of pastries you'd like to order. Only whole pastries please: ");
       int numPastry = int.Parse(Console.ReadLine());
+      Console.WriteLine("---------------------------");
       Bread brad = new Bread();
-      Console.WriteLine(brad.BreadCost(numBread));
+      Console.WriteLine("Your total cost of Bread is $" + brad.BreadCost(numBread));
       Pastry pat = new Pastry();
-      Console.WriteLine(pat.PastryCost(numPastry));
-
+      Console.WriteLine("Your total cost of Pastry is $" + pat.PastryCost(numPastry));
+      Console.WriteLine("---------------------------");
+      Console.WriteLine("Your total cost of is $"+ brad.BreadCost(numBread) + pat.PastryCost(numPastry));
     }
   }
 }
